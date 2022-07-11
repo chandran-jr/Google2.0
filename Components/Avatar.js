a690d6b25fd254dcb5365f31b1e4008f84180d3c
Govind B Chandran <govindchandran150@gmail.com>
2022-07-11 23:11:57 +0530

    Add files via upload

--- Components/Avatar.js
+++ Components/Avatar.js
@@ -2,7 +2,7 @@
 
 function Avatar({url}) {
   return (
-    <img loading="lazy" src={url} alt="profile" />
+    <img className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110" loading="lazy" src={url} alt="profile" />
   )
 }
 
