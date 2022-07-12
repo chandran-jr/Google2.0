3e85bbfc5ed281e827d4d80aff2f6b9ede26755f
Govind B Chandran <govindchandran150@gmail.com>
2022-07-12 19:29:51 +0530

    Add files via upload

--- Components/Socialbutton.js
+++ Components/Socialbutton.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Socialbutton({text,col,borderCol,url,imgUrl}) {
   return (
     <a href={url}>
-    <div className={`flex items-center justify-evenly h-14 w-32 rounded-xl cursor-pointer ${col} border-4 ${borderCol}`}>
+    <div className={`lg:h-14 lg:w-32 lg:flex-row flex flex-col items-center justify-evenly h-2 w-8 rounded-xl cursor-pointer ${col} border-4 ${borderCol}`}>
         <img className="h-6 text-white" src={imgUrl} alt="logo" />
-        <p className="text-white text-lg">{text}</p>
+        <p className="invisible lg:visible text-white text-lg">{text}</p>
     </div>
     </a>
   )
