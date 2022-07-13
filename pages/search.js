1ea0ea2e4180070e13bbef5b9db9905e3018c0c5
Govind B Chandran <govindchandran150@gmail.com>
2022-07-13 13:29:25 +0530

    Add files via upload

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,11 @@
 import Head from "next/head"
 import HeaderSearch from "../Components/HeaderSearch"
+import {API_KEY, CONTEXT_KEY} from "../keys";
 
-function Search() {
+function Search({results}) {
+
+    console.log(results);
+
   return (
     <div>
         <Head>
@@ -15,3 +19,18 @@ function Search() {
 }
 
 export default Search
+
+
+export async function getServerSideProps(context) {
+    const useDummyData  = false;
+
+    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json());
+
+    return {
+        props: {
+            results: data
+        }
+    }
+
+
+}
